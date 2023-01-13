5b93b8093cdb3a611f768ce4bdcb517ccef340c3
MaciejKorniak <maciejkorniak07@gmail.com>
2023-01-14 00:02:50 +0100

    ObjectPool v0.1

--- Assets/ObjectPooler.cs
+++ Assets/ObjectPooler.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
-    public Dictionary<string, Queue<GameObject>>
+    [System.Serializable]
+    public class Pool{
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
     // Start is called before the first frame update
     void Start()
     {
-        
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach (Pool pool in pools){
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
     }
 
 }
