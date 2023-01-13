d0969b40a5dd8cf6aa41a98aaea32b9f38a04595
MaciejKorniak <maciejkorniak07@gmail.com>
2023-01-13 23:19:56 +0100

    First real commit

---
+++ Assets/ObjectPooler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    public Dictionary<string, Queue<GameObject>>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
